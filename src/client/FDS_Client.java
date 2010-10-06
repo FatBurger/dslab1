01a1363468114ea1899cfe32f91b29bc6204042f
Hugo Habicht <fat.burger@gmx.at>
2010-10-06 18:02:08 +0200

    Added basic skeleton for FDS_Client

---
+++ src/client/FDS_Client.java
@@ -0,0 +1,19 @@
+package client;
+
+/**
+ * The main class for the File Distribution System (FDS) Client
+ * 
+ * @author Raphael Mader, 0826052
+ */
+public class FDS_Client
+{
+   /**
+    * Main entry point
+    * 
+    * @param args The command line arguments
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World");
+   }
+}
